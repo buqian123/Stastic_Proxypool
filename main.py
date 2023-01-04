563b43accdf61fa42826dca92f6ea8731467673f
RealLaptop <tx@littlesite.ml>
2023-01-04 13:48:14 +0800

    first commit

--- main.py
+++ main.py
@@ -13,6 +13,10 @@ p = subprocess.Popen('./proxypool -c ./config/config.yaml', shell=True)
 #持续监测运行并检测是否完成
 while 1:
     if p.returncode != 0:
+        try:
+          p.kill()
+          p.terminate()
+        except: print("Kill Error")
         p = subprocess.Popen('sudo ./proxypool -c ./config/config.yaml', shell=True)
     try:
         sub = requests.get("http://localhost:8080/clash/proxies")
@@ -29,6 +33,10 @@ f = open("config.yaml", "x")
 f.write(proxies)
 f.close()
 #清理
+try:
+  p.kill()
+  p.terminate()
+except: print("Kill Error")
 os.remove("./assets/GeoLite2-City.mmdb")
 
 
